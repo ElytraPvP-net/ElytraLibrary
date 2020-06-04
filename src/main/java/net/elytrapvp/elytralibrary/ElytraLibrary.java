5a83aca4d17f52244f9b7cc8ab93999d0b1f8865
Colby <colbykrawczyk10@gmail.com>
2020-06-04 02:27:06 -0400

    Added Initial Files

---
+++ src/main/java/net/elytrapvp/elytralibrary/ElytraLibrary.java
@@ -0,0 +1,14 @@
+package net.elytrapvp.elytralibrary;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ElytraLibrary extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+
+    }
+
+}
